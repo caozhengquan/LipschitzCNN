9faf4a4268d477781b2f9386ab36084b730ee76e
zoumath <dzou@ima.umn.edu>
2017-12-13 11:49:53 -0600

    modify getlip

--- GoogleNet/getLip.m
+++ GoogleNet/getLip.m
@@ -9,16 +9,67 @@
 % figure('Units','normalized','Position',[0.1 0.1 0.8 0.8]);
 % plot(lgraph);
 
-layer = net.Layers(8,1);
-W = layer.Weights;
+% gNet = {};
+% j = 0;
+% for i = [2 6 8 12:2:20 26:2:34 41:2:49 55:2:63 69:2:77 83:2:91 97:2:105, ...
+%         112:2:120 126:2:134]
+%     j = j + 1;
+%     gNet{j,1} = net.Layers(i).Name;
+%     gNet{j,2} = net.Layers(i).Stride(1);
+%     gNet{j,3} = net.Layers(i).Weights;
+% end
+%     
 
+% layer = net.Layers(8,1);
+% W = layer.Weights;
+% 
+% 
+% Delta = layer.Stride;
+% Delta = Delta(1);
+% N = size(W, 1);
+% C = size(W, 3);
+% D = size(W, 4);
+% ND = ceil(N/Delta);
 
-Delta = layer.Stride;
-Delta = Delta(1);
-N = size(W, 1);
-C = size(W, 3);
-D = size(W, 4);
+load('gNet.mat');
+LL = [];
+for t = 4:5:44
+
+ind_start = t; % where 1-by-1 is
+W0 = gNet{ind_start, 3};
+C0 = size(W0, 3);
+D0 = size(W0, 4);
+W1 = gNet{ind_start+2, 3};
+C1 = size(W1, 3);
+D1 = size(W1, 4);
+W2 = gNet{ind_start+4, 3};
+C2 = size(W2, 3);
+D2 = size(W2, 4);
+W0 = padarray(W0, [4,4], 'post');
+W1 = padarray(W1, [2,2], 'post');
+C = C0 + C1 + C2;
+D = D0 + D1 + D2;
+W = zeros(5, 5, C, D);
+W(:, :, 1:C0, 1:D0) = W0;
+W(:, :, C0+1:C0+C1, D0+1:D0+D1) = W1;
+W(:, :, C0+C1+1:C, D0+D1+1:D) = W2;
+N = 5;
+Delta = 1;
 ND = ceil(N/Delta);
+
+
+
+% ind_start = t; % where 1-by-1 is
+% W1 = gNet{ind_start+1, 3};
+% C = size(W1, 3);
+% Dtemp = size(W1, 4);
+% W2 = gNet{ind_start+3, 3};
+% W0 = reshape(eye(C), [1,1,C,C]);
+% W = cat(4,W0,W1,W2);
+% Delta = 1;
+% D = size(W,4);
+% ND = ceil(N/Delta);
+
 
 M = zeros(ND, ND, D, C*Delta*Delta);
 
@@ -54,14 +105,6 @@ L = max(Mnorm(:));
 L = [L size(W)];
 L = [L Delta];
         
+LL = [LL; L];
 
-% gNet = {};
-% j = 0;
-% for i = [2 6 8 12:2:20 26:2:34 41:2:49 55:2:63 69:2:77 83:2:91 97:2:105, ...
-%         112:2:120 126:2:134]
-%     j = j + 1;
-%     gNet{j,1} = net.Layers(i).Name;
-%     gNet{j,2} = net.Layers(i).Stride(1);
-%     gNet{j,3} = net.Layers(i).Weights;
-% end
-%     
+end
